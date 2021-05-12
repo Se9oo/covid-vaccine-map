5e3354566cf555a343b20883c0a9a545d6f7d616
shpark <shpark@jness.co.kr>
2021-05-12 18:03:07 +0900

    marker, overlay 생성 로직 개선

--- front/src/components/VaccineMap.jsx
+++ front/src/components/VaccineMap.jsx
@@ -1,70 +1,121 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { locationData } from '../data/data';
 import styled from 'styled-components';
 
 const { kakao } = window;
 
-const VaccineMap = () => {
-  useEffect(() => {
-    kakao.maps.load(() => {
-      const mapContainer = document.getElementById('map');
-      const options = {
-        //center: new kakao.maps.LatLng(37.506502, 127.053617),
-        center: new kakao.maps.LatLng(37.567817, 127.004501),
-        level: 5,
-      };
+// kakao map 생성
+function loadKakaoMap(setKakaoMap) {
+  const mapContainer = document.getElementById('map');
 
-      const map = new kakao.maps.Map(mapContainer, options);
+  const options = {
+    center: new kakao.maps.LatLng(37.567817, 127.004501),
+    level: 5,
+  };
 
-      locationData.data.forEach((data) => {
-        // marker setting
-        const marker = new kakao.maps.Marker({
-          position: new kakao.maps.LatLng(data.lng, data.lat),
-          title: data.centerName,
-          clickable: true,
-        });
+  const map = new kakao.maps.Map(mapContainer, options);
 
-        marker.setMap(map);
+  setKakaoMap(map);
+}
 
-        // overlay setting
-        const overlay = new kakao.maps.CustomOverlay({
-          content: `
-            <div class="Container">
-              <div class="OverlayContainer">
-                <div class="title">
-                  <h2>
-                    ${data.facilityName}
-                  </h2>
-                </div>
-                <div class="content">
-                  <dl>
-                    <div class="content-item">
-                      <dt>우편번호</dt>
-                      <dd>${data.zipCode}</dd>
-                    </div>
-                    <div class="content-item">
-                      <dt>주소</dt>
-                      <dd>${data.address}</dd>
-                    </div>
-                  </dl>
-                </div>
-              </div>
-              <div class="triangle"></div>
-            </div>
-          `,
-          position: new kakao.maps.LatLng(data.lng, data.lat),
-          yAnchor: 1.3,
-          isClicked: false,
-        });
+// 지도에 그릴 marker, overlay 생성
+function createMarkerAndOverlay(mapData = '', setMarkers, setOverlays) {
+  const markerData = mapData === '' ? locationData.data : mapData;
+  const markerArray = [];
+  const overlayArray = [];
 
-        kakao.maps.event.addListener(marker, 'click', function () {
-          overlay.isClicked ? overlay.setMap(null) : overlay.setMap(map);
-
-          overlay.isClicked = !overlay.isClicked;
-        });
-      });
+  markerData.map((data) => {
+    const marker = new kakao.maps.Marker({
+      position: new kakao.maps.LatLng(data.lng, data.lat),
+      title: data.centerName,
+      clickable: true,
     });
+
+    const overlay = new kakao.maps.CustomOverlay({
+      content: `
+        <div class="Container">
+          <div class="OverlayContainer">
+            <div class="title">
+              <h2>
+                ${data.facilityName}
+              </h2>
+            </div>
+            <div class="content">
+              <dl>
+                <div class="content-item">
+                  <dt>우편번호</dt>
+                  <dd>${data.zipCode}</dd>
+                </div>
+                <div class="content-item">
+                  <dt>주소</dt>
+                  <dd>${data.address}</dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+          <div class="triangle"></div>
+        </div>
+      `,
+      position: new kakao.maps.LatLng(data.lng, data.lat),
+      yAnchor: 1.3,
+      isClicked: false,
+    });
+
+    markerArray.push(marker);
+    overlayArray.push(overlay);
+  });
+
+  setMarkers(markerArray);
+  setOverlays(overlayArray);
+}
+
+// 기존에 지도상에 있던 marker, overlay 제거
+function clearMarkerAndOverlay(setMarkers, setOverlays) {
+  setMarkers([]);
+  setOverlays([]);
+}
+
+// 지도에 marker, overlay 세팅
+function setMarkerAndOverlay(map, markers = [], overlays = []) {
+  if (!map) {
+    return;
+  }
+
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+
+    kakao.maps.event.addListener(markers[i], 'click', function () {
+      overlays[i].isClicked ? overlays[i].setMap(null) : overlays[i].setMap(map);
+
+      overlays[i].isClicked = !overlays[i].isClicked;
+    });
+  }
+}
+
+const VaccineMap = ({ mapData = '' }) => {
+  const [kakaoMap, setKakaoMap] = useState(null);
+  const [markers, setMarkers] = useState([]);
+  const [overlays, setOverlays] = useState([]);
+
+  // 초기 실행 (지도 생성)
+  useEffect(() => {
+    loadKakaoMap(setKakaoMap);
   }, []);
+
+  // 지도 생성시 지도에 그릴 marker, overlay 생성
+  useEffect(() => {
+    createMarkerAndOverlay(mapData, setMarkers, setOverlays);
+  }, [kakaoMap]);
+
+  // marker, overlay 생성되면 지도에 marker, overlay 세팅
+  useEffect(() => {
+    setMarkerAndOverlay(kakaoMap, markers, overlays);
+  }, [markers, overlays]);
+
+  // 지역 검색으로 mapData가 바뀌면 기존 marker, overlay 제거
+  useEffect(() => {
+    clearMarkerAndOverlay(setMarkers, setOverlays);
+  }, [mapData]);
 
   return <MapContainer id="map"></MapContainer>;
 };
