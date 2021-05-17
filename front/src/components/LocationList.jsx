1051d2171b1c2a242c64a49c9ea09bf44259eb50
park se hyeon <se9oo@kakao.com>
2021-05-17 22:20:30 +0900

    예방접종 병원리스트 컴포넌트 추가

---
+++ front/src/components/LocationList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { locationData } from '../data/data';
+import styled from 'styled-components';
+import LocationListItem from './LocationListitem';
+
+const LocationList = ({ mapData = '', setSelectLocation }) => {
+  const data = mapData === '' ? locationData.data : mapData;
+
+  return (
+    <StyledList>
+      {data.map((location) => {
+        return <LocationListItem key={location.id} location={location} setSelectLocation={setSelectLocation} />;
+      })}
+    </StyledList>
+  );
+};
+
+export default LocationList;
+
+const StyledList = styled.ul`
+  display: none;
+
+  @media ${(props) => props.theme.laptop} {
+    display: block;
+    width: 100%;
+  }
+`;
---
+++ front/src/components/LocationListitem.jsx
@@ -0,0 +1,27 @@
+import React, { useCallback } from 'react';
+import styled from 'styled-components';
+
+const LocationListItem = ({ location, setSelectLocation }) => {
+  const onClickLocation = useCallback(() => {
+    setSelectLocation(location);
+  }, []);
+
+  return (
+    <StyledListItem onClick={onClickLocation}>
+      <Title>{location.facilityName}</Title>
+    </StyledListItem>
+  );
+};
+
+export default LocationListItem;
+
+const StyledListItem = styled.li`
+  width: 100%;
+  padding: 2rem 0;
+  border-bottom: 1px solid #e5e5e5;
+  cursor: pointer;
+`;
+
+const Title = styled.strong`
+  font-size: 1.2rem;
+`;
